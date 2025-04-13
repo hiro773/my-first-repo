94ca57c44bb76e132fb89dc48a723238b4b7e1b4
Your Namehiro <hirot1357@icloud.com>
2025-04-13 22:42:27 +0900

    リストの基本操作

--- practice.py
+++ practice.py
@@ -1,5 +1,7 @@
-def aisatu(name):
-    print( name + "さん" +"お金をください")
-
-aisatu("Nara")
-aisatu("Hiro")
+fruits = ["apple","banana","orange"]
+fruits.append("grape")
+fruits.remove("banana")
+fruits.insert(2,"kiwi")
+print(len(fruits))
+for fruit in fruits:
+    print(fruit)
