fddd83513ea0a7f5baec7e0e27d2aa41634c8e20
Your Namehiro <hirot1357@icloud.com>
2025-04-15 00:29:02 +0900

    ファイルの書き込み

---
+++ read_file.py
@@ -0,0 +1,3 @@
+with open("intro.txt" , "r" , encoding="utf-8") as file:
+    content = file.read()
+    print(content)
---
+++ write_file.py
@@ -0,0 +1,3 @@
+with open("intro.txt","w",encoding="utf-8") as file:
+    file.write("名前:山田太郎\n")
+    file.write("趣味:スノボー\n")
